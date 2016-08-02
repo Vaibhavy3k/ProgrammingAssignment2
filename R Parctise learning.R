27216bff3bee11fc7c3e6c48dac19ac55ee263b6
Vaibhavy3k <vaibhavs625@gmail.com>
2016-08-02 18:45:26 +0530

    This is for my personal practise
    for personal R practise

---
+++ R Parctise learning.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <-function(x=matrix()){
+m<-NULL
+set<-function(y) {
+x<<-y
+m<<-NULL
+}
+get<-function() x
+setmean<-function(solve) m<<-mean
+getmean<-function() m
+list(set = set, get = get,
+setmean = setmean,
+getmean = getmean)
+}
+
+cachesolve<-function(x,....){
+m<-x$getmean()
+if(!is.null(m)){
+message("getting cached data")
+return(m)
+}
+data<-x$get()
+m<-solve(data,...)
+x$setmean(m)
+m
+}
